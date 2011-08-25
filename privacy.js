640a9109102a45cbbe052aea557bea630d6e92c3
Soumya Deb <debloper@gmail.com>
2011-08-25 20:06:26 +0530

    Let's create the container elements (crude method)

--- privacy.js
+++ privacy.js
@@ -4,3 +4,15 @@
 
 privacy = {};
 privacy.NODE = document.getElementsByTagName("privacy").item(0);
+
+privacy.newNode = function (id) {
+	var e = document.createElement("div");
+	e.setAttribute("id", id[i]);
+	return e;
+}
+privacy.provision = privacy.newNode("provision");
+privacy.perversion = privacy.newNode("perversion");
+privacy.persistence = privacy.newNode("persistence");
+privacy.NODE.appendChild(privacy.provision);
+privacy.NODE.appendChild(privacy.perversion);
+privacy.NODE.appendChild(privacy.persistence);
