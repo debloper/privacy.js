e8164cfd2c5810d858ec04b441952e7ed38a897e
Soumya Deb <debloper@gmail.com>
2011-08-25 22:00:02 +0530

    Let's take it to the next level baby! :P

--- privacy.js
+++ privacy.js
@@ -4,8 +4,12 @@
 
 privacy = {};
 privacy.NODE = document.getElementsByTagName("privacy").item(0);
+
 privacy.ids = {};
 privacy.ids.main = [ "provision", "perversion", "persistence" ];
+privacy.ids.provision = [ "provision_main" ];
+privacy.ids.perversion = [ "perversion_top", "perversion_middle", "perversion_bottom" ];
+privacy.ids.persistence = [ "persistence_1st", "persistence_2nd", "persistence_3rd", "persistence_4th" ];
 
 privacy.newNode = function (id) {
 	var e = document.createElement("div");
@@ -19,3 +23,10 @@ privacy.setupNodes = function (ids, parent) {
 	}
 }
 privacy.setupNodes(privacy.ids.main, privacy.NODE);
+
+privacy.PROVISION = document.getElementById("provision");
+privacy.PERVERSION = document.getElementById("perversion");
+privacy.PERSISTENCE = document.getElementById("persistence");
+privacy.setupNodes(privacy.ids.provision, privacy.PROVISION);
+privacy.setupNodes(privacy.ids.perversion, privacy.PERVERSION);
+privacy.setupNodes(privacy.ids.persistence, privacy.PERSISTENCE);
